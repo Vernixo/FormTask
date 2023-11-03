d1c262cdffa33a8301358b0faf2dc187c5782c63
Mateusz Michalik <mateuszmichalikxtr@gmail.com>
2023-11-03 21:45:31 +0100

    Add project files.

---
+++ Controllers/DaneController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Formularz.Models;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Formularz.Controllers
+{
+    public class DaneController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult Form()
+        {
+            return View();
+        }
+        [HttpPost]
+        [HttpPost]
+        public IActionResult Form(Dane dane)
+        {
+            if (ModelState.IsValid)
+            {
+                return View("Wynik", dane);
+            }
+            else return View();
+
+        }
+
+        public IActionResult Wynik(Dane dane)
+        {
+
+            return View(dane);
+        }
+
+    }
+}
---
+++ Models/Dane.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Formularz.Models
+{
+    public class Dane
+    {
+        [Required(ErrorMessage = "Prosze podaj Imie")]
+        public string Imie { get; set; }
+        public string Nazwisko { get; set; }
+        [Required(ErrorMessage = "Prosze podaj Email")]
+        [RegularExpression(".+\\@.+\\.[a-z]{2,3}")]
+        public string Email { get; set; }
+        //[Required(ErrorMessage = "Prosze podaj Temat")]
+        //public string Temat { get; set; }
+        //[Required(ErrorMessage = "Prosze podaj Tresc wiadomosci")]
+        //[MinLength(10), MaxLength(50)]
+        //public string Tresc { get; set; }
+        public string Haslo { get; set; }
+        public string HasloPotwierdzenie { get; set; }
+        public int NumerTelefonu { get; set; }
+        public int Wiek { get; set; }
+        public string Miasto { get; set; }
+        public enum Miasta { Bydgoszcz = 1, Radom = 2, Białystok = 3, Marianów = 4, Węgrzce = 5 }
+
+    }
+}
